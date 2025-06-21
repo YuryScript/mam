9ef52195d1ebd18fbcb7d7685e72427af4e34682
YuryScript <yuratorn@gmail.com>
2025-06-21 17:53:57 +0300

    бредик с темами

---
+++ ys/mathDarts/mathDarts.view.ts
@@ -0,0 +1,9 @@
+namespace $.$$ {
+	export class $ys_mathDarts extends $.$ys_mathDarts {
+
+		@ $mol_mem_key
+		Columns(key: number, next ?: number[]) {
+			return new Array(4).map((r) => false)
+		}
+	}
+}
